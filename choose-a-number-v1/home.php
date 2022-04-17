66aa0a6dbc44cf74754b4b275a3e4a8a9fdfbdec
mohammedsanta <94539550+mohammedsanta@users.noreply.github.com>
2022-04-17 03:16:59 +0200

    choose-a-number-v1

---
+++ choose-a-number-v1/home.php
@@ -0,0 +1,30 @@
+
+<?php include_once("number.php")?>
+
+<html>
+<head>
+
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <title>Shoose Number</title>
+
+</head>
+
+<body>
+
+    <h1>اختار رقم في سرك</h1>
+
+    <p>اضربو في 2</p>
+
+    <p>زود عليه <?php echo $secret;?></p>
+
+    <p>اقسمو علي ال 2</p>
+
+    <p>اطرح الناتج من الرقم اللي اخترتو</p>
+
+    <p>الناتج هو ---------> <?php echo $secret_equal; ?> </p>
+
+
+</body>
+
+</html>
---
+++ choose-a-number-v1/number.php
@@ -0,0 +1,5 @@
+<?php
+$secret = rand(1,100);
+
+
+$secret_equal = $secret / 2;
